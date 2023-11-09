a40e20dfdbea8852f529e0d66fa7de7d1bb4337f
Walker Crystal <36904927+nitrospaz@users.noreply.github.com>
2023-11-09 12:02:33 -0700

    added comments, look for all json files rel dir

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿var salesFiles = FindFiles("stores");
+﻿var currentDirectory = Directory.GetCurrentDirectory();
+
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesFiles = FindFiles(storesDirectory);
 
 foreach (var file in salesFiles)
 {
@@ -13,8 +17,8 @@ IEnumerable<string> FindFiles(string folderName)
 
     foreach (var file in foundFiles)
     {
-        // The file name will contain the full path, so only check the end of it
-        if (file.EndsWith("sales.json"))
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
         {
             salesFiles.Add(file);
         }
@@ -22,3 +26,27 @@ IEnumerable<string> FindFiles(string folderName)
 
     return salesFiles;
 }
+
+
+// // Path builds and parses strings
+// Console.WriteLine($"stores{Path.DirectorySeparatorChar}201");
+// Console.WriteLine(Path.Combine("stores","201"));
+// // returns:
+// // stores\201 on Windows
+// //
+// // stores/201 on macOS
+
+// Console.WriteLine(Path.GetExtension("sales.json")); // outputs: .json
+
+
+// string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+// FileInfo info = new FileInfo(fileName);
+// Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}"); // And many more
+
+
+// Console.WriteLine(Directory.GetCurrentDirectory());
+
+// string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+// Console.WriteLine(docPath);
+
+// Console.WriteLine($"Number of files found: {salesFiles.Count()}");
